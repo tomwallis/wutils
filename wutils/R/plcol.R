0abe20bcd64c586e05698fa9a78eb86d721fe5b1
Tom Wallis <tomwallis@eap104200.extern.uni-tuebingen.de>
2013-10-14 10:49:12 +0200

    adjusted the plcol function to accept and return the ggplot object.

--- wutils/R/plcol.R
+++ wutils/R/plcol.R
@@ -1,43 +1,47 @@
-#' Generate Tom's favourite plot colours.
+#' Add Tom's favourite plot colours to a ggplot object.
 #' 
-#' This function can be called with no inputs to generate Tom's commonly used colour palettes for plotting.
-#' These are returned as character arrays in the workspace for use in adding scales.
+#' This function accepts a ggplot object and returns the object with the colour and fill scales
+#' adjusted to the specified colour scheme.
+#' 
 #' @export  
 #' 
 #' @details
 #' Possible arguments: 
 #' \itemize{
 #' \item \code{'pleasing'}: return hand-tuned pleasing colours.
-#' \item \code{'cbPalette'}: return colourblind friendly palette with grey.
-#' \item \code{'cbbPalette'}: return colourblind friendly palette with black.
+#' \item \code{'cb'}: return colourblind friendly palette.
 #' \item \code{'greyscale'}: return greyscale palette.
 #' }
 #' 
-#' @return A character vector that can be passed to ggplot2's manual colour scales.
+#' @return returns a ggplot2 object with adjusted manual colour scale and fill scale.
 #' 
+#' @param fig a ggplot2 object.
 #' @param palette a string argument for which colour palette to return.
+#' @param ... further arguments passed to scale_colour_manual and scale_fill_manual (e.g. could pass name='Scale name').
+#'
 #' @author Thomas Wallis (with colours collected from around the web)
 #' @examples
+#' library(ggplot2)
 #' data(diamonds)
 #' diamonds <- diamonds[sample(nrow(diamonds),size=2000),]
+#' diamonds <- subset(diamonds,cut==c('Good','Very Good','Premium','Ideal'))
 #' fig <- ggplot(diamonds,aes(x=carat,y=price,colour=cut)) + geom_point()
-#' my_colours <- plcol(palette='cbbPalette')
-#' fig <- fig + scale_colour_manual(values=my_colours)
+#' fig <- plcol(fig,palette='pleasing')
 #' fig
 #' 
 #' # change to greyscale:
-#' my_colours <- plcol(palette='greyscale')
-#' fig <- fig + scale_colour_manual(values=my_colours)
+#' fig <- plcol(fig,palette='greyscale') 
 #' fig
 
-plcol <- function(palette='cbPalette'){
+plcol <- function(fig,palette='cb',...){
   # hand-tuned pleasing colours
-  if(palette == 'pleasing') return(c("#3C4884","#DD5525","#8C2212","#E18942","#425227","#94117E"))
+  if(palette == 'pleasing') plcol <- c('#A6CEE3','#1F78B4','#B2DF8A','#33A02C')
   # Colourblind-friendly palette with grey:
-  if(palette == 'cbPalette') return(c("#999999", "#E69F00", "#56B4E9", "#009E73","#0072B2", "#D55E00", "#CC79A7","#F0E442"))
-  # Colourblind-friendly palette with black:
-  if(palette == 'cbbPalette') return(c("#000000", "#E69F00", "#56B4E9", "#009E73", "#0072B2", "#D55E00", "#CC79A7","#F0E442"))
+  if(palette == 'cb') plcol <- c("#E69F00", "#56B4E9", "#009E73","#0072B2", "#D55E00", "#CC79A7","#F0E442")
   # greyscale palette:
-  if(palette == 'greyscale') return(c('#222222','#555555','#777777','#999999','#bbbbbb','#dddddd'))  
+  if(palette == 'greyscale') plcol <- c('#222222','#555555','#777777','#999999','#bbbbbb','#dddddd')
+  
+  fig <- fig + scale_colour_manual(values=plcol,...) + scale_fill_manual(values=plcol,...)
+  return(fig)
 }
 
